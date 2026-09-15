775b2897e7372fce38e0c7f1008248a7793f171f
khankadir193 <abdulkadirk059@gmail.com>
2026-09-15 19:31:01 +0530

    Add OS module examples demonstrating system information retrieval

---
+++ operatingSystem/operatingSystem.js
@@ -0,0 +1,17 @@
+import os from 'os';
+
+console.log('platform:-',os.platform());
+console.log('architecture:-',os.arch());
+console.log('cpus :-',os.cpus());
+console.log('home dir:-',os.homedir());
+console.log('home dir:-',os.hostname());
+console.log('os tmp dir:-',os.tmpdir());
+console.log('total memory:-',os.totalmem());
+console.log('free memory :-',os.freemem());
+console.log('up time :-',os.uptime());
+console.log('os types :-',os.type());
+console.log('os release :-',os.release());
+console.log('os user info :-',os.userInfo());
+console.log('os interface :-',os.networkInterfaces());
+console.log('end of line char :-',os.EOL);
+
